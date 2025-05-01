11b583a2717361249b51a0f0d62ca380cfd2a120
Simon-B-Williams <150749940+Simon-B-Williams@users.noreply.github.com>
2025-05-01 15:00:22 +0100

    Update main.tf
    remove insecure ssh allow resource

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -13,19 +13,6 @@ resource "aws_default_security_group" "default" {
   }
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound from anywhere"
-  vpc_id      = aws_vpc.example.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "allow_ssh_with_valid_cidr" {
   name        = "allow_ssh_with_valid_cidr"
   description = "Allow SSH inbound from specific range"
